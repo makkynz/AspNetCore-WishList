469dcb5c90d74bc92c96568363ad7dcadac48b55
Anthony Marshall <anthonymarshall@gmail.com>
2019-09-09 17:56:05 +1200

    sdfsf

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,7 +32,7 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseMvc();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
